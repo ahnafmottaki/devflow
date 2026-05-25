adb7d322841c74c8283d7fda2f4d775755d18501
ahnafmottaki <ahnafmottaki2022@gmail.com>
2026-05-25 21:29:36 +0600

    setup prettier and theming in tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Homepage() {
   return (
     <>
-      <h1 className="text-3xl text-bold underline">
-        Welcome to the ultimate nextjs course
-      </h1>
+      <h1 className="text-bold text-3xl underline">Welcome to the ultimate nextjs course</h1>
     </>
   );
 }
