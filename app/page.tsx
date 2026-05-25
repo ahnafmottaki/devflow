51e07bf54cd7c03b94d1722f3509dd81173b7e92
ahnafmottaki <ahnafmottaki2022@gmail.com>
2026-05-25 21:58:14 +0600

    add assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Homepage() {
   return (
     <>
-      <h1 className="text-bold text-3xl underline">Welcome to the ultimate nextjs course</h1>
+      <h1 className="text-bold font-inter text-3xl underline">Welcome to the ultimate nextjs course</h1>
+      <h1 className="text-bold font-space-grotesk text-3xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi unde at sit odio deserunt tempore, id error earum
+        modi quas repellat consequuntur placeat, ullam excepturi libero dignissimos non maiores reiciendis, dicta
+        corporis? Tempore commodi, reprehenderit eum velit perspiciatis inventore voluptatem esse, laborum,
+        exercitationem tenetur ullam facilis nulla placeat vitae? Voluptatibus.
+      </h1>
     </>
   );
 }
